b337d62630a27a9a7af53545fc95d5c0116c5814
Rainer Brieger <hello@rainerb.com>
2020-12-22 08:28:47 -0700

    debugging

--- src/components/Search.js
+++ src/components/Search.js
@@ -32,7 +32,7 @@ const Search = () => {
                 clearTimeout(timeoutId);
             }
         };
-    }, [term]);
+    }, [term, results.length]);
 
     const renderedResults = results.map((result) => {
         return (
